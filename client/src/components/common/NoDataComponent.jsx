03a318bf827d24eb0aebb958eea9dd8c40bdf4a3
Elad Gubkin <Eladox3@gmail.com>
2019-07-22 18:31:11 +0300

    No data for react-table dark & light

---
+++ client/src/components/common/NoDataComponent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const NoDataComponent = props => {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        display: 'block',
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+        background: 'transparent',
+        zIndex: 1,
+        pointerEvents: 'none',
+        padding: '20px'
+      }}
+    >
+      <h3>\(o_o)/</h3>
+      <p className="text-flow">No rows found!</p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  currentTheme: state.settings.currentTheme
+});
+
+NoDataComponent.propTypes = {
+  currentTheme: PropTypes.string.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(NoDataComponent);
