3d4be60a401a40b42ae8d38e635cd7659d7957cf
Elad Gubkin <Eladox3@gmail.com>
2019-08-01 17:57:07 +0300

    Added UPDATE_COMPUTERS_PING type

--- client/src/state/ducks/computer/types.js
+++ client/src/state/ducks/computer/types.js
@@ -3,5 +3,5 @@ You can use any convention you wish here, but the name should remain UPPER_SNAKE
 */
 
 const SEARCH_COMPUTERS = 'computer/SEARCH_COMPUTERS';
-
-export { SEARCH_COMPUTERS };
+const UPDATE_COMPUTERS_PING = 'computer/UPDATE_COMPUTERS_PING';
+export { SEARCH_COMPUTERS, UPDATE_COMPUTERS_PING };
