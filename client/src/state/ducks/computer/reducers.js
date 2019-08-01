44db1536582d8ed049ff57a6cca32d53249bafc3
Elad Gubkin <Eladox3@gmail.com>
2019-08-01 17:59:02 +0300

    filtable payload for computers, still need a fix

--- client/src/state/ducks/computer/reducers.js
+++ client/src/state/ducks/computer/reducers.js
@@ -4,16 +4,59 @@ Based on the state shape, multiple reducers might be defined in this file, combi
 */
 
 import * as types from './types';
+import { dbComputers } from './db';
 
 const initState = {
-  computers: []
+  computers: dbComputers
 };
 
 const computer = (state = initState, action) => {
   switch (action.type) {
     case types.SEARCH_COMPUTERS:
       return {
-        computers: action.payload.computers
+        ...state,
+        computers: [...state.computers, ...action.payload.computers].reduce(
+          (acc, current) => {
+            const x = acc.find(item => item.mac === current.mac);
+            if (!x) {
+              return acc.concat([current]);
+            } else {
+              return acc;
+            }
+          },
+          []
+        )
+        // computers: action.payload.computers
+      };
+
+    case types.UPDATE_COMPUTERS_PING:
+      // console.log(
+      //   action.payload.computers.reduce((a, b) => {
+      //     let a1 = state.computers.find(e => e.mac === b.mac) || {};
+      //     return a.concat(Object.assign(a1, b));
+      //   }, [])
+      // );
+
+      // console.log(
+      //   action.payload.computers.reduce((a, b) => {
+      //     let a1 = state.computers.find(e => e.mac === b.mac) || {};
+      //     return a.concat(Object.assign(a1, b));
+      //   }, [])
+      // );
+
+      // .reduce((a, b) => {
+      //   let a1 = state.computers.find(e => e.mac === b.mac) || {};
+      //   return a.concat(Object.assign(a1, b));
+      // }, [])
+
+      // const macs = new Set([state.computers].map(d => d.mac));
+
+      return {
+        ...state,
+        computers: action.payload.computers.reduce((a, b) => {
+          let a1 = state.computers.find(e => e.mac === b.mac) || {};
+          return a.concat(Object.assign(a1, b));
+        }, [])
       };
 
     default:
