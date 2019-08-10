46e58c1d4e1d65b0ca6de1d858cb25dc75d6e428
Elad Gubkin <Eladox3@gmail.com>
2019-08-10 18:32:10 +0300

    Lock

--- server/agent.py
+++ server/agent.py
@@ -1,11 +1,13 @@
 import json
 import uuid
 from cnc.command_factory import CommandAnswerFactory
+from asyncio import Lock
 
 class Agent(object):
     def __init__(self, ws):
         self.id = str(uuid.uuid4())
         self.ws = ws
+        self.lock = Lock()
     
     async def send(self, data):
         await self.ws.send(json.dumps(data))
