a09a8e993ae56893637876a4e61e8b90e7e72faa
Elad Gubkin <Eladox3@gmail.com>
2019-08-12 07:29:52 +0300

    download & upload & moment.js

--- client/src/state/ducks/computer/actions.js
+++ client/src/state/ducks/computer/actions.js
@@ -5,7 +5,8 @@ HINT: Always use functions for consistency, don't export plain objects
 
 import * as types from './types';
 import Agent from '../../../utils/agent';
-import { timeFormat } from '../../../utils/timeFormat';
+// import { timeFormat } from '../../../utils/timeFormat';
+import moment from 'moment';
 
 if (!window.agent) {
   window.agent = new Agent();
@@ -16,46 +17,59 @@ const agent = window.agent;
 const searchComputers = (protocol, agentId, data) => dispatch => {
   switch (protocol) {
     case 'snmp':
-      const command = agent.transfer(agentId, agent.snmpScan(data.network, data.community));
+      const command = agent.transfer(
+        agentId,
+        agent.snmpScan(data.network, data.community)
+      );
       console.log('Sending SCAN command, command ID = ', command.commandId);
-      agent
-        .execute(
-          command    
-        )
-        .then(({ commandAnswer: { result } }) => {
-          const foundComputersObj = result;
-          const foundComputersArr = [];
-          console.log(foundComputersObj);
-          
-          for (const [ip] of Object.entries(foundComputersObj)) {
-            foundComputersArr.push({
-              name: foundComputersObj[ip].name,
-              ip: ip,
-              mac: foundComputersObj[ip].interfaces.find(
-                item => item.description === 'eth0.4'
-              ).mac,
-              ping: 0,
-              uptime: timeFormat(foundComputersObj[ip].uptime),
-              description: foundComputersObj[ip].description,
-              location: foundComputersObj[ip].location,
-              interfaces: foundComputersObj[ip].interfaces
-            });
-          }
+      agent.execute(command).then(({ commandAnswer: { result } }) => {
+        const foundComputersObj = result;
+        const foundComputersArr = [];
+        console.log(foundComputersObj);
 
-          agent.execute(agent.getComputers()).then((computers) => {
-            const macs = new Set([].map(d => d.mac));
-            const noDupComputers = [
-              ...computers,
-              ...foundComputersArr.filter(d => !macs.has(d.mac))
-            ];
-  
-            foundComputersArr.reduce((a, b) => {
-              let a1 = noDupComputers.find(e => e.mac === b.mac) || {};
-              return a.concat(Object.assign(a1, b));
-            }, []);
-  
-            // computers = noDupComputers;
-            agent.execute(agent.saveComputers(JSON.stringify(noDupComputers))).then(() => {
+        for (const [ip] of Object.entries(foundComputersObj)) {
+          foundComputersArr.push({
+            name: foundComputersObj[ip].name,
+            ip: ip,
+            mac: foundComputersObj[ip].interfaces.find(
+              item => item.description === 'eth0.4'
+            ).mac,
+            ping: 0,
+            // uptime: timeFormat(foundComputersObj[ip].uptime),
+            uptime: moment
+              .utc(foundComputersObj[ip].uptime * 1000)
+              .format('DD:HH:mm'),
+            description: foundComputersObj[ip].description,
+            location: foundComputersObj[ip].location,
+            upload: (
+              foundComputersObj[ip].interfaces.find(
+                item => item.description === 'eth0.4'
+              ).out_octets / 10000000
+            ).toFixed(2),
+            download: (
+              foundComputersObj[ip].interfaces.find(
+                item => item.description === 'eth0.4'
+              ).in_octets / 10000000
+            ).toFixed(2)
+            // interfaces: foundComputersObj[ip].interfaces
+          });
+        }
+
+        agent.execute(agent.getComputers()).then(computers => {
+          const macs = new Set([].map(d => d.mac));
+          const noDupComputers = [
+            ...computers,
+            ...foundComputersArr.filter(d => !macs.has(d.mac))
+          ];
+
+          foundComputersArr.reduce((a, b) => {
+            let a1 = noDupComputers.find(e => e.mac === b.mac) || {};
+            return a.concat(Object.assign(a1, b));
+          }, []);
+
+          agent
+            .execute(agent.saveComputers(JSON.stringify(noDupComputers)))
+            .then(() => {
               dispatch({
                 type: types.SEARCH_COMPUTERS,
                 payload: {
@@ -63,8 +77,21 @@ const searchComputers = (protocol, agentId, data) => dispatch => {
                 }
               });
             });
-          })
         });
+      });
+
+      break;
+
+    case 'tcp':
+      const tcpCommand = agent.transfer(
+        agentId,
+        agent.tcpScan(data.network, data.ports)
+      );
+      console.log('Sending SCAN command, command ID = ', tcpCommand.commandId);
+
+      agent.execute(tcpCommand).then(({ commandAnswer: { result } }) => {
+        console.log(result);
+      });
 
       break;
 
@@ -74,8 +101,8 @@ const searchComputers = (protocol, agentId, data) => dispatch => {
   }
 };
 
-const pingComputers = (network, agentId, computers) => dispatch => {
-  return new Promise((resolve, reject) => {
+const pingComputers = (network, agentId) => dispatch => {
+  return new Promise(resolve => {
     agent
       .execute(agent.transfer(agentId, agent.ping(network)))
       .then(({ commandAnswer: { result } }) => {
@@ -85,12 +112,12 @@ const pingComputers = (network, agentId, computers) => dispatch => {
         });
         resolve();
       });
-    });
+  });
 };
 
 const getComputers = () => dispatch => {
-  return new Promise((resolve, reject) => {
-    agent.execute(agent.getComputers()).then(({computers}) => {
+  return new Promise(resolve => {
+    agent.execute(agent.getComputers()).then(({ computers }) => {
       dispatch({
         type: types.SEARCH_COMPUTERS,
         payload: {
@@ -100,17 +127,19 @@ const getComputers = () => dispatch => {
       resolve(JSON.parse(computers));
     });
   });
-}
+};
 
-const saveComputers = (computers) => dispatch => {
-  agent.execute(agent.saveComputers(JSON.stringify(computers))).then(({computers}) => {
-    dispatch({
-      type: types.SEARCH_COMPUTERS,
-      payload: {
-        computers
-      }
-    })
-  })
-}
+const saveComputers = computers => dispatch => {
+  agent
+    .execute(agent.saveComputers(JSON.stringify(computers)))
+    .then(({ computers }) => {
+      dispatch({
+        type: types.SEARCH_COMPUTERS,
+        payload: {
+          computers
+        }
+      });
+    });
+};
 
 export { searchComputers, pingComputers, getComputers, saveComputers };
